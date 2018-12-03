f636eb1fc19f264d0f295c82008a0ca9f6e26000
fweezee <jordangana23@gmail.com>
2018-12-04 03:47:08 +0800

    sounds sounds

---
+++ Assets/Projects/finalProj/Scripts/PlaySound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    public AudioClip attack_sound;
+
+    public AudioSource audioS;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void attacksound()
+    {
+        audioS.PlayOneShot(attack_sound);
+    }
+}
