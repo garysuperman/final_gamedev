7dcac12a1bb6dc93760e1e3bd143e008cf966851
Garysuperman <garynon12@gamil.com>
2018-12-03 09:51:45 +0800

    added wolf script

---
+++ Assets/Projects/finalProj/Enemines/Grunts/wolf_script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wolf_script : MonoBehaviour {
+    //stats
+    [SerializeField] private int health = 500;
+
+    //speed = 0/idle, 1/run, 2/attack 1, 3/attack 2
+    private const string speed = "speed";
+
+    // 0 = right, 1 = left
+    private int facing = 0;
+    private const float TURN_AMOUNT_MODIFIER = 500.0f;
+
+
+    [SerializeField] private Animator wolf_anim;
+    [SerializeField] private GameObject wolf;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Collider collider;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
