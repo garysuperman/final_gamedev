a38178e78ea9ced2331922a04eacc79e161972b4
Garysuperman <garynon12@gamil.com>
2018-12-03 21:13:53 +0800

    fixed player movement; added more to the level

---
+++ Assets/Projects/finalProj/Level/DamageZone Scripts/PitfallB.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitfallB : MonoBehaviour {
+
+    [SerializeField] Camera cam;
+
+    void OnTriggerEnter(Collider collider) {
+        Vector3 moving;
+        if (collider.gameObject.name.Contains("link")){
+            cam.GetComponent<followPlayer>().lever();
+            moving = collider.gameObject.transform.localPosition;
+            moving.x = 2.34f;
+            moving.y = 35f;
+            moving.z = -22.93f;
+            collider.gameObject.transform.localPosition = moving;
+        }
+    }
+}
--- Assets/Projects/finalProj/Scripts/followPlayer.cs
+++ Assets/Projects/finalProj/Scripts/followPlayer.cs
@@ -8,21 +8,33 @@ public class followPlayer : MonoBehaviour {
     private float x_diff = 0;
     private float y_diff = 0;
     private float z_diff = 0;
+    private bool enabled = true;
+
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         Vector3 pos = player.transform.position;
         x_diff = pos.x - transform.position.x;
         y_diff = pos.y - transform.position.y;
         z_diff = pos.z - transform.position.z;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 pos = player.transform.position;
-        pos.x -= x_diff;
-        pos.y -= y_diff;
-        pos.z -= z_diff;
-        transform.position = pos;
+
+    // Update is called once per frame
+    void Update() {
+        if (enabled) {
+            Vector3 pos = player.transform.position;
+            pos.x -= x_diff;
+            pos.y -= y_diff;
+            pos.z -= z_diff;
+            transform.position = pos;
+        }
+        
     }
+
+    public void  lever(){
+        if (enabled)
+            enabled = false;
+        else enabled = true;
+    }
+
 }
