74a70c929769e64a7e64bef35fb686e05d4e96de
Garysuperman <garynon12@gamil.com>
2018-12-06 12:09:24 +0800

    added post processing

--- Assets/Projects/finalProj/Enemines/Bosses/treant_script.cs
+++ Assets/Projects/finalProj/Enemines/Bosses/treant_script.cs
@@ -93,7 +93,6 @@ public class treant_script : MonoBehaviour {
             this.treant_anim.SetInteger(speed, 0);
             if (healthBar.gameObject.activeSelf)
             {
-                Debug.Log("in");
                 healthBar.gameObject.SetActive(false);
                 nameText.gameObject.SetActive(false);
             }
